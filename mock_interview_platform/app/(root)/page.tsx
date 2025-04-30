ed9cf595a9305bdc00bb63cf28457fdf524a7556
Harsh-986 <hy025u@gmail.com>
2025-04-30 21:06:21 +0530

    day2 Done with Login and Signup Page

--- mock_interview_platform/app/(root)/page.tsx
+++ mock_interview_platform/app/(root)/page.tsx
@@ -1,8 +1,20 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>Home Page</div>
+    <>
+    <section className='card-cta'>
+      <div className='flex flex-col gap-6 max-w-lg'>
+        <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+        <p className='text-lg'>Practice job interviews with AI and get instant feedback to improve your performance.</p>
+        <Button asChild className='btn-primary max-sm:w-full'>
+          <Link href="/interview">Start an Interview</Link>
+        </Button>
+      </div>
+    </section>
+    </>
   )
 }
 
--- mock_interview_platform/components/AuthForm.tsx
+++ mock_interview_platform/components/AuthForm.tsx
@@ -1,8 +1,108 @@
-import React from 'react'
+"use client"
 
-const AuthForm = () => {
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import {z} from "zod"
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+import Link from "next/link"
+import { toast } from "sonner"
+import { Form } from "./ui/form"
+import FormField from "./FormField"
+import { useRouter } from "next/navigation"
+
+
+
+const authFornSchema = (type: FormType) => {
+  return z.object({
+    name :type === "sign-up" ? z.string().min(3, { message: "Name is required" }) : z.string().optional(),
+    email: z.string().email({ message: "Email is required" }),
+    password: z.string().min(3, { message: "Password is required" }),
+})}
+
+const AuthForm = ({type} : {type:FormType}) => {
+  const router = useRouter()
+  const formSchema = authFornSchema(type)
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      if(type === "sign-up") {
+        toast.success("Account created successfully")
+        router.push("/sign-in")
+      }
+      else {
+        // Sign in logic here
+        toast.success("Logged in successfully")
+        router.push("/")
+      }
+    } catch (error) {
+      console.error(error)
+      toast.error(`Threre was an error: ${error}`) 
+    }
+  }
+
+  const inSignIn = type === "sign-in";
+
+
   return (
-    <div>AuthForm</div>
+    <div className="card-border lg:min-w-[566px]">
+      <div className="flex flex-col gap-6 card py-14 px-10">
+        <div className="flex flex-row gap-2 justify-center">
+          <Image src="/logo.svg" alt="logo" height={32} width={38}/>
+          <h2 className="text-primary-100">PrepWise</h2>        
+        </div>
+
+        <h3>Practice job Interview with AI</h3>
+      
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-4 mt-4 form">
+            {!inSignIn && 
+            (<FormField 
+              control={form.control} 
+              name="name" 
+              label="Name" 
+              placeholder="Your Name" 
+              type="text"
+            />)}
+            <FormField 
+              control={form.control} 
+              name="email" 
+              label="Email" 
+              placeholder="Your Email" 
+              type="email"
+            />
+            <FormField 
+              control={form.control} 
+              name="password" 
+              label="Password" 
+              placeholder="Enter Your Password" 
+              type="password"
+            />
+            <Button className="btn" type="submit">{inSignIn ? "Sign in" : "Create an Account"}</Button>
+          </form>
+        </Form>
+
+        <p className="text-center">
+          {inSignIn ? "Don't have an account?" : "Already have an account?"}{" "}
+          <Link href={!inSignIn ? '/sign-in' : '/sign-up'} className="font-bold text-user-primary ml-1">
+            {inSignIn ? "Sign up" : "Sign in"}
+          </Link>
+          
+        </p>
+
+      </div>
+    </div>
   )
 }
 
