21bfcd6e4bf04e9776a6ae0d1310fd113b54fe36
Harsh-986 <hy025u@gmail.com>
2025-05-06 12:47:11 +0530

    day 8.2 final project

--- mock_interview_1/firebase/client.ts
+++ mock_interview_1/firebase/client.ts
@@ -10,13 +10,13 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCPsNMmGBkJYwCS-cSUXEt-vEomtPjqw4Q",
-  authDomain: "prewise-cba3d.firebaseapp.com",
-  projectId: "prewise-cba3d",
-  storageBucket: "prewise-cba3d.firebasestorage.app",
-  messagingSenderId: "834528633258",
-  appId: "1:834528633258:web:75dc2e39f292215a138bd3",
-  measurementId: "G-SSKF6P6W08"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
